9f04955701aa5a8663b10bb805fc43f69e18f47b
ronavarr <roberto.navarra@everis.com>
2021-01-12 22:24:54 +0100

    Link rest call to button

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Button, SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
+import Axios, {AxiosInstance} from 'axios';
+import Slider from '@react-native-community/slider';
 
 const Separator = () => (
     <View style={styles.separator}/>
@@ -10,21 +12,37 @@ export default class ButtonBasics extends Component {
     private min: number = 0;
     private max: number = 100;
 
+    private basePath: string = 'https://localhost/';
+    private timeout: number = 3000;
+
+    private axios : AxiosInstance = Axios.create({
+        baseURL: this.basePath,
+        timeout: this.timeout,
+        headers: {
+            Accept: '*/*',
+        }
+    });
+
     constructor(props: {} | Readonly<{}>) {
-        super(props)
-        this.state = {text: String(this.volume)};
+        super(props);
+        this.state = {text: this.volume};
+
+        this.load();
+    }
+
+    load() {
+        this._getVolume();
     }
 
     _onPressButton() {
         alert('Changed value!');
     }
 
-    _onChangeValue = (value: string) => {
+    _onChangeValue = (value: number) => {
         try {
-            const number = Number(value);
-            if (number >= 0 && number <= 100) {
+            if (value >= 0 && value <= 100) {
                 this.setState({text: value})
-                this.volume = Number(value);
+                this.volume = value;
             } else {
                 alert('The value not good! Set a value from 0 to 100')
             }
@@ -35,7 +53,7 @@ export default class ButtonBasics extends Component {
 
     _decreaseVolume = () => {
         if (this.volume > this.min) {
-            this.setState({text: String(this.volume - 1)})
+            this.setState({text: this.volume - 1})
             this.volume -= 1
         } else {
             alert('Cannot decrease volume, min is reached!')
@@ -44,28 +62,73 @@ export default class ButtonBasics extends Component {
 
     _increaseVolume = () => {
         if (this.volume < this.max) {
-            this.setState({text: String(this.volume + 1)})
+            this.setState({text: this.volume + 1})
             this.volume += 1
         } else {
             alert('Cannot increase volume, max is reached!')
         }
     }
 
+    _activeMute = () => {
+        this.axios.put(this.basePath + 'volume/controller/muteOn')
+            .then((response: any ) => {
+                alert(response.data.message);
+            })
+            .catch((error: any) => {
+                alert(error.message + " " + error.code);
+            });
+    }
+
+    _disableMute = () => {
+        this.axios.put('volume/controller/muteOff')
+            .then((response: any ) => {
+                alert(response.data.message);
+            })
+            .catch((error: any) => {
+                alert(error.message + " " + error.code);
+            });
+    }
+
+    _setVolume = () => {
+        this.axios.put('volume/controller/changeVolume',
+            {
+                volume: this.volume
+            })
+            .then((response: any ) => {
+                alert(response.data.message);
+            })
+            .catch((error: any) => {
+                alert(error.message + " " + error.code);
+            });
+    }
+
+    _getVolume = () => {
+        this.axios.get('volume/controller/getVolume')
+            .then((response: any ) => {
+                this.volume = response.data.volume;
+                this.setState({text: this.volume})
+            })
+            .catch((error: any) => {
+                alert(error.message + " " + error.code);
+            });
+    }
+
     render() {
         return (
             <SafeAreaView style={styles.container}>
-                <View>
-                    <Text style={styles.title}>
-                        The title and onPress handler are required. It is recommended to set accessibilityLabel to help
-                        make your app usable by everyone.
-                    </Text>
+                <View style={styles.header}>
+                    <View>
+                        <Text>
+                            Current Volume
+                        </Text>
+                    </View>
+                    <View>
+                        <Text style={styles.volumeTitle}>
+                            {this.volume + '%'}
+                        </Text>
+                    </View>
                 </View>
                 <Separator/>
-                <View>
-                    <Text style={styles.title}>
-                        Volume setter interaction.
-                    </Text>
-                </View>
                 <View>
                     <View style={styles.fixToText}>
                         <View style={[{width: "20%"}]}>
@@ -75,10 +138,15 @@ export default class ButtonBasics extends Component {
                             />
                         </View>
                         <View style={styles.fixToText}>
-                            <TextInput
-                                onChangeText={this._onChangeValue}
+                            <Slider
+                                style={{width: 200, height: 40}}
+                                minimumValue={0}
+                                maximumValue={100}
+                                minimumTrackTintColor={'#2198f2'}
+                                thumbTintColor={'#2198f2'}
                                 value={this.state.text}
-                                textAlign={'center'}
+                                step={1}
+                                onValueChange={this._onChangeValue}
                             />
                         </View>
                         <View style={[{width: "20%"}]}>
@@ -89,51 +157,38 @@ export default class ButtonBasics extends Component {
                         </View>
                     </View>
                 </View>
-                <Separator/>
                 <View>
                     <View style={styles.title}>
                         <Button
-                            title="Set Volume"
-                            onPress={this._onPressButton}
+                            title="Apply"
+                            onPress={this._setVolume}
+                        />
+                    </View>
+                </View>
+                <Separator/>
+                <View style={styles.fixToText}>
+                    <View style={[{width: "30%"}]}>
+                        <Button
+                            title="Mute On"
+                            onPress={this._activeMute}
+                        />
+                    </View>
+                    <View style={[{width: "30%"}]}>
+                        <Button
+                            title="Mute Off"
+                            onPress={this._disableMute}
+                        />
+                    </View>
+                </View>
+                <View style={styles.footer}>
+                    <View style={[{width: "50%"}]}>
+                        <Button
+                            title="Refresh"
+                            onPress={this._getVolume}
                         />
                     </View>
                 </View>
             </SafeAreaView>
-            /*
-            <View style={styles.container}>
-                <View>
-                    <Text>Actual value</Text>
-                    <Text>{this.volume}</Text>
-                </View>
-                <View>
-                    <View style={styles.component}>
-                        <NumericInput
-                            value={this.state.text}
-                            minValue={this.min}
-                            maxValue={this.max}
-                            onChange={this._onChangeText}
-                            onLimitReached={(max, msg) => alert(this.msgMax)}
-                            totalWidth={240}
-                            totalHeight={50}
-                            iconSize={25}
-                            step={1}
-                            valueType='integer'
-                            rounded
-                            textColor='#000000'
-                            iconStyle={{color: 'white'}}
-                            rightButtonBackgroundColor='#0066FF'
-                            leftButtonBackgroundColor='#0066FF'/>
-                    </View>
-                    <View style={styles.component}>
-                        <Button
-                            onPress={this._onPressButton}
-                            title="Set"
-                            color={'#0066FF'}
-                        />
-                    </View>
-                </View>
-            </View>
-            */
         );
     }
 }
@@ -156,5 +211,35 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         borderBottomColor: '#737373',
         borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    textInputStyle: {
+        textAlign : 'center',
+        height: 40,
+        borderBottomWidth: 2,
+        borderBottomColor: '#2198f2',
+        marginBottom: 10
+    },
+    volumeTitle: {
+        fontSize: 40,
+        fontFamily: 'Arial, sans-serif'
+    },
+    header: {
+        width: '100%',
+        position: 'absolute',
+        top: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center'
+    },
+    footer: {
+        width: '100%',
+        position: 'absolute',
+        bottom: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
 });
