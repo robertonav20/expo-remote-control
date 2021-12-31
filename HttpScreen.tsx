7ddc51ff1a060bcdb6a27f61932818bad03c75fc
ConsNavarraRoberto <cons.NavarraRoberto@sia.eu>
2021-12-31 01:48:41 +0100

    Add a list and modal into http screen

--- HttpScreen.tsx
+++ HttpScreen.tsx
@@ -1,23 +1,88 @@
-import {View} from "react-native";
+import {FlatList, Modal, Pressable, SafeAreaView, Text, TouchableHighlight, View} from "react-native";
+import React, {useState} from 'react';
 import {httpScreenStyle} from "./Styles";
-import React, {Component} from "react";
+import {ButtonGroup} from "react-native-elements";
+import {AntDesign, Feather} from "@expo/vector-icons";
 
-export default class HttpScreen extends Component<{}, {}> {
 
-    constructor(props: Readonly<any>) {
-        super(props);
-    }
+const HttpScreen = () => {
+    let data = [
+        {
+            id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+            title: 'First Item',
+        },
+        {
+            id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+            title: 'Second Item',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            title: 'Third Item',
+        }
+    ];
 
-    render() {
-        return (
-            <View style={httpScreenStyle.mainContainer}>
-                <View style={httpScreenStyle.headerContainer}>
+    const [httpData, setHttpData] = useState(data);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalElement, setModalElement] = useState(null);
+    const actions = (item: any, value: number) => {
+        console.log(item, value)
+        if (value === 0) {
+            setModalElement(item);
+            setModalVisible(true);
+        } else if (value === 1) {
+            setHttpData(httpData.filter(d => d.id !== item.id));
+        }
+    };
+    const showButton = () => (
+        <Feather name="info" size={25} color="white" style={httpScreenStyle.infoButton}/>
+    );
+
+    const clearButton = () => (
+        <AntDesign name="delete" size={25} color="white" style={httpScreenStyle.deleteButton}/>
+    );
+
+    const renderItem = (element: any) => (
+        <View style={httpScreenStyle.item}>
+            <Text style={httpScreenStyle.title}>
+                {element.item.title}
+            </Text>
+            <ButtonGroup
+                buttons={[{element: showButton}, {element: clearButton}]}
+                onPress={(value) => actions(element.item, value)}
+                containerStyle={{borderRadius: 5}}>
+            </ButtonGroup>
+        </View>
+    );
+
+    return (
+        <View style={httpScreenStyle.centeredView}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+            >
+                <View style={httpScreenStyle.centeredView}>
+                    <View style={httpScreenStyle.modalView}>
+                        <Text style={httpScreenStyle.modalText}>
+                            { modalElement != null ? modalElement?.title : null}
+                        </Text>
+                        <TouchableHighlight
+                            style={[httpScreenStyle.infoButton, httpScreenStyle.deleteButton]}
+                            onPress={() => setModalVisible(false)}
+                        >
+                            <Text style={httpScreenStyle.textStyle}>Hide Modal</Text>
+                        </TouchableHighlight>
+                    </View>
                 </View>
-                <View style={httpScreenStyle.middleContainer}>
-                </View>
-                <View style={httpScreenStyle.footerContainer}>
-                </View>
-            </View>
-        )
-    }
+            </Modal>
+            <FlatList
+                data={httpData}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                style={httpScreenStyle.list}
+            />
+        </View>
+    );
 }
+
+export default HttpScreen;
--- Styles.tsx
+++ Styles.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
 
 export const commonStyles = StyleSheet.create({
     pickerContainer: {
@@ -328,15 +328,82 @@ export const configurationScreenStyle = StyleSheet.create({
 
 export const httpScreenStyle = StyleSheet.create({
     mainContainer: {
+        alignItems: 'center',
         flex: 1,
         flexGrow: 3,
         flexDirection: 'column',
-        alignItems: 'center',
         justifyContent: 'space-around',
+        paddingTop: StatusBar.currentHeight,
     },
     headerContainer: {},
     middleContainer: {},
-    footerContainer: {}
+    footerContainer: {},
+    deleteButton: {
+        backgroundColor: '#f91b20',
+        padding: 10
+    },
+    infoButton: {
+        backgroundColor: '#2198f2',
+        padding: 10
+    },
+    list: {
+        width: '100%'
+    },
+    item: {
+        backgroundColor: 'white',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 10,
+        marginVertical: 8,
+        marginHorizontal: 10
+    },
+    title: {
+        fontSize: 20,
+        color: '#2198f2',
+        fontFamily: 'Candara'
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+        backgroundColor: "#2196F3",
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+    }
 })
 
 export const appScreenStyles = StyleSheet.create({
